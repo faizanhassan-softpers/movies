53bbe493e12b85b068da30a631557b0a10c8ce0e
Hassan <hassanabbas@team.softpers.com>
2024-08-05 00:44:35 +0500

    feat: complete the integration in create movie page

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -1,9 +1,14 @@
 'use client';
 
+import { getLoggedInUserId } from "@/utils/functions";
 import Image from "next/image";
+import { redirect } from "next/navigation";
+import { useRouter } from "next/router";
 import { useState } from 'react';
 
 const Page = () => {
+  const router = useRouter();
+
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [title, setTitle] = useState('');
   const [publishingYear, setPublishingYear] = useState('');
@@ -21,12 +26,55 @@ const Page = () => {
     setPublishingYear(event.target.value);
   }
 
-  const handleSubmit = () => {
-    console.log('====================================');
-    console.log('selectedFile: ', selectedFile);
-    console.log('title: ', title);
-    console.log('publishingYear: ', publishingYear);
-    console.log('====================================');
+  const handleSubmit = async () => {
+    if (selectedFile !== null && title !== '' && publishingYear !== '') {
+      try {
+        // Upload the file and get the path
+        const arrayBuffer = await selectedFile?.arrayBuffer();
+
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/api/upload`,
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/octect-stream',
+            },
+            body: arrayBuffer
+          });
+
+        if (response.ok) {
+          const data = await response.json();
+          const imageUrl = data.path;
+
+          const owner = await getLoggedInUserId();
+
+          const createMovieData = {
+            owner,
+            title,
+            publishingYear: Number(publishingYear),
+            imageUrl
+          };
+
+          const result = await fetch(
+            `${process.env.NEXT_PUBLIC_BASE_URL}/api/movies`,
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(createMovieData)
+            });
+        } else {
+          console.log('====================================');
+          console.log('Error uploading the image');
+          console.log('====================================');
+        }
+      } catch (error) {
+        console.log('====================================');
+        console.log('Error: ', error);
+        console.log('====================================');
+      }
+    }
   }
 
   return (
@@ -35,7 +83,7 @@ const Page = () => {
       <div className="flex flex-col lg:flex-row mt-10 lg:mt-20">
         <div className="h-64 w-full lg:w-[473px] lg:h-[504px] border-2 rounded-md border-dotted border-white bg-Input flex items-center justify-center">
           <div className="flex flex-col items-center">
-            <input type="file" className="hidden" onChange={handleFileChange} />
+            <input type="file" onChange={handleFileChange} />
             <Image
               src="/svgs/download.svg"
               alt="Upload"
